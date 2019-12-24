ccd647314b89403b1580cd13c81782df494d7631
Gleb Mozhaiskii <gleb_mozhaiskii@epam.com>
2019-12-24 20:26:45 +0300

    removed .idea

--- 5/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gleb/Desktop/SP_2019/5
-# Build directory: /home/gleb/Desktop/SP_2019/5/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(MergeSortTest "src/test/MergeSortTest.cpp")
-set_tests_properties(MergeSortTest PROPERTIES  _BACKTRACE_TRIPLES "/home/gleb/Desktop/SP_2019/5/CMakeLists.txt;24;add_test;/home/gleb/Desktop/SP_2019/5/CMakeLists.txt;0;")
