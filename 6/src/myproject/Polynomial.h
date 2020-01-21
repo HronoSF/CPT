fcd449692a76d35b6a177f9d7387b8fdbea3c091
HronoSF <gleb_mozhaiskii@epam.com>
2020-01-21 19:57:35 +0300

    fixed memory leak in Poltnomial.h

--- 6/src/myproject/Polynomial.h
+++ 6/src/myproject/Polynomial.h
@@ -1,110 +1,125 @@
 #include <complex>
+#include <memory>
 
 template<class T>
-class Polynomial
-{
+class Polynomial {
 private:
-    T* array; // array[0] = a(n), array[size-1] = a(0)
+    std::unique_ptr<T[]> array; // array[0] = a(n), array[size-1] = a(0)
     int size;
 
     template<class Arg>
-    void setArgs(int place, Arg head){
+    void setArgs(int place, Arg head) {
         array[place] = head;
     }
 
     template<class Arg>
-    void setArgs(int place, Arg head, Arg tail){
+    void setArgs(int place, Arg head, Arg tail) {
         array[place] = head;
-        array[place+1] = tail;
+        array[place + 1] = tail;
     }
 
     template<class Head, class ... Args>
-    void setArgs(int place, Head head, Args ... tail){
+    void setArgs(int place, Head head, Args ... tail) {
         array[place] = head;
-        voidSetArgs(place+1, tail...);
+        voidSetArgs(place + 1, tail...);
     }
 
 public:
+    explicit Polynomial() = delete;
+
+    Polynomial(const Polynomial &) = delete;
+
+    Polynomial(Polynomial &&data) : array(std::move(data.array)) {
+        size = data.size;
+    }
+
+    Polynomial &operator=(Polynomial &&data) {
+        size = std::move(data.size);
+        array = std::move(data.array);
+        return *this;
+    }
+
+    ~Polynomial() = default;
+
     template<class ... Args>
-    Polynomial(T head, Args ... args){
-        size = sizeof...(args)+1;
-        array = new T[size];
+    Polynomial(T head, Args ... args):  array(new T[sizeof...(args) + 1]{std::forward<Args>(args)...}) {
+        size = sizeof...(args) + 1;
         array[0] = head;
         setArgs(1, args...);
     }
 
-    Polynomial(T* arr, int size){
-        this->array = arr;
-        this->size = size;
+    Polynomial(T *arr, int size) {
+        array.reset(arr);
+        std::swap(this->size, size);
     }
 
-    int getSize() const{
+    int getSize() const {
         return size;
     }
 
-    void multiply(T x){
-        for(int i=0; i<size; i++){
-            array[i] = array[i]*x;
+    void multiply(T x) {
+        for (int i = 0; i < size; i++) {
+            array[i] = array[i] * x;
         }
     }
 
-    T getArg(int place) const{
+    T getArg(int place) const {
         return array[place];
     }
 
 };
 
 template<typename T>
-struct is_complex_t : public std::false_type{};
+struct is_complex_t : public std::false_type {
+};
 
 template<typename T>
-struct is_complex_t<std::complex<T>> : public std::true_type{};
+struct is_complex_t<std::complex<T>> : public std::true_type {
+};
 
 template<typename T>
-constexpr bool is_complex(){ return is_complex_t<T>::value;}
+constexpr bool is_complex() { return is_complex_t<T>::value; }
 
 template<class T>
 typename std::enable_if<std::is_arithmetic<T>::value, Polynomial<T>>::type
-schurTransform(Polynomial<T> p){
+schurTransform(Polynomial<T> p) {
 
     int size = p.getSize();
-    T* array = new T[size];
-    for(int i=0; i<size; i++){
-        array[i] = p.getArg(size-1-i);
+    T *array = new T[size];
+    for (int i = 0; i < size; i++) {
+        array[i] = p.getArg(size - 1 - i);
     }
 
-    Polynomial<T> reciprocal = Polynomial<T>(array, size);
+    Polynomial<T> reciprocal = std::move(Polynomial<T>(array, size));
 
     reciprocal.multiply(p.getArg(0));
-    p.multiply(p.getArg(p.getSize()-1));
+    p.multiply(p.getArg(p.getSize() - 1));
 
-    T* schurArray = new T[size];
-
-    for(int k=0; k<size; k++){
+    T *schurArray = new T[size];
+    for (int k = 0; k < size; k++) {
         schurArray[k] = p.getArg(k) - reciprocal.getArg(k);
     }
 
-    return Polynomial<T>(schurArray, size);
+    return std::move(Polynomial<T>(schurArray, size));
 }
 
 template<class T>
 typename std::enable_if<is_complex_t<T>::value, Polynomial<T>>::type
-schurTransform(Polynomial<T> p){
+schurTransform(Polynomial<T> &p) {
 
     int size = p.getSize();
-    T* array = new T[size];
-    for(int i=0; i<size; i++){
-        array[i] = std::conj(p.getArg(size-1-i));
+    T *array = new T[size];
+    for (int i = 0; i < size; i++) {
+        array[i] = std::conj(p.getArg(size - 1 - i));
     }
 
-    Polynomial<T> reciprocal = Polynomial<T>(array, size);
+    Polynomial<T> reciprocal(Polynomial<T>(array, size));
 
     reciprocal.multiply(p.getArg(0));
-    p.multiply(std::conj(p.getArg(p.getSize()-1)));
+    p.multiply(std::conj(p.getArg(p.getSize() - 1)));
 
-    T* schurArray = new T[size];
-
-    for(int k=0; k<size; k++){
+    T *schurArray = new T[size];
+    for (int k = 0; k < size; k++) {
         schurArray[k] = p.getArg(k) - reciprocal.getArg(k);
     }
 
--- 6/src/test/PolynomialTest.cc
+++ 6/src/test/PolynomialTest.cc
@@ -2,77 +2,77 @@
 #include "../myproject/Polynomial.h"
 #include <complex>
 
-TEST(POLYNOMIAL, CONSTRUCTOR_TEST){
-Polynomial<int> p = Polynomial<int>(1, 2);
+TEST(POLYNOMIAL, CONSTRUCTOR_TEST) {
+    Polynomial<int> p(1, 2);
 
-EXPECT_EQ(1, p.getArg(0));
-EXPECT_EQ(2, p.getArg(1));
+    EXPECT_EQ(1, p.getArg(0));
+    EXPECT_EQ(2, p.getArg(1));
 }
 
-TEST(POLYNOMIAL, MULTIPLY_TEST){
-Polynomial<int> p = Polynomial<int>(1, 2);
-p.multiply(2);
+TEST(POLYNOMIAL, MULTIPLY_TEST) {
+    Polynomial<int> p(1, 2);
+    p.multiply(2);
 
-EXPECT_EQ(2, p.getArg(0));
-EXPECT_EQ(4, p.getArg(1));
+    EXPECT_EQ(2, p.getArg(0));
+    EXPECT_EQ(4, p.getArg(1));
 }
 
-TEST(POLYNOMIAL, SCHUR_TEST){
-    Polynomial<int> p = Polynomial<int>(1, 2, 3);
+TEST(POLYNOMIAL, SCHUR_TEST) {
+    Polynomial<int> p(1, 2, 3);
 
-    Polynomial<int> schurPoly = schurTransform(p);
+    Polynomial<int> schurPoly = schurTransform(std::move(p));
 
     EXPECT_EQ(schurPoly.getArg(0), 0);
     EXPECT_EQ(schurPoly.getArg(1), 4);
     EXPECT_EQ(schurPoly.getArg(2), 8);
 }
 
-TEST(POLYNOMIAL, COMPLEX_NUMBERS_TEST){
-std::complex<double> first = std::complex<double>(1, 1);
-std::complex<double> second = std::complex<double>(2, 2);
+TEST(POLYNOMIAL, COMPLEX_NUMBERS_TEST) {
+    std::complex<double> first = std::complex<double>(1, 1);
+    std::complex<double> second = std::complex<double>(2, 2);
 
-std::complex<double> conjFirst = std::conj(first);
-std::complex<double> conjSecond = std::conj(second);
+    std::complex<double> conjFirst = std::conj(first);
+    std::complex<double> conjSecond = std::conj(second);
 
-EXPECT_EQ(std::real(conjFirst), std::real(first));
-EXPECT_EQ(std::imag(conjFirst), -1*std::imag(first));
-EXPECT_EQ(std::real(conjSecond), std::real(second));
-EXPECT_EQ(std::imag(conjSecond), -1*std::imag(second));
+    EXPECT_EQ(std::real(conjFirst), std::real(first));
+    EXPECT_EQ(std::imag(conjFirst), -1 * std::imag(first));
+    EXPECT_EQ(std::real(conjSecond), std::real(second));
+    EXPECT_EQ(std::imag(conjSecond), -1 * std::imag(second));
 
-std::complex<double> multi = first*second;
-EXPECT_EQ(std::real(multi), 0);
-EXPECT_EQ(std::imag(multi), 4);
+    std::complex<double> multi = first * second;
+    EXPECT_EQ(std::real(multi), 0);
+    EXPECT_EQ(std::imag(multi), 4);
 }
 
-TEST(POLYNOMIAL, SCHUR_2COMPLEX_TEST){
-std::complex<double> first = std::complex<double>(1, 1);
-std::complex<double> second = std::complex<double>(2, 2);
+TEST(POLYNOMIAL, SCHUR_2COMPLEX_TEST) {
+    std::complex<double> first = std::complex<double>(1, 1);
+    std::complex<double> second = std::complex<double>(2, 2);
 
-Polynomial<std::complex<double>> p =
-        Polynomial<std::complex<double>>(first, second);
+    Polynomial<std::complex<double>> p =
+            Polynomial<std::complex<double>>(first, second);
 
-Polynomial<std::complex<double>> schurPoly = schurTransform(p);
+    Polynomial<std::complex<double>> schurPoly = schurTransform(p);
 
-EXPECT_EQ(std::real(schurPoly.getArg(0)), 0);
-EXPECT_EQ(std::imag(schurPoly.getArg(0)), 0);
-EXPECT_EQ(std::real(schurPoly.getArg(1)), 6);
-EXPECT_EQ(std::imag(schurPoly.getArg(1)), 0);
+    EXPECT_EQ(std::real(schurPoly.getArg(0)), 0);
+    EXPECT_EQ(std::imag(schurPoly.getArg(0)), 0);
+    EXPECT_EQ(std::real(schurPoly.getArg(1)), 6);
+    EXPECT_EQ(std::imag(schurPoly.getArg(1)), 0);
 }
 
-TEST(POLYNOMIAL, SCHUR_3COMPLEX_TEST){
-std::complex<double> first = std::complex<double>(1, 1);
-std::complex<double> second = std::complex<double>(2, 2);
-std::complex<double> third = std::complex<double>(3, 3);
+TEST(POLYNOMIAL, SCHUR_3COMPLEX_TEST) {
+    std::complex<double> first = std::complex<double>(1, 1);
+    std::complex<double> second = std::complex<double>(2, 2);
+    std::complex<double> third = std::complex<double>(3, 3);
 
-Polynomial<std::complex<double>> p =
-        Polynomial<std::complex<double>>(first, second, third);
+    Polynomial<std::complex<double>> p =
+            Polynomial<std::complex<double>>(first, second, third);
 
-Polynomial<std::complex<double>> schurPoly = schurTransform(p);
+    Polynomial<std::complex<double>> schurPoly = schurTransform(p);
 
-EXPECT_EQ(std::real(schurPoly.getArg(0)), 0);
-EXPECT_EQ(std::imag(schurPoly.getArg(0)), 0);
-EXPECT_EQ(std::real(schurPoly.getArg(1)), 8);
-EXPECT_EQ(std::imag(schurPoly.getArg(1)), 0);
-EXPECT_EQ(std::real(schurPoly.getArg(2)), 16);
-EXPECT_EQ(std::imag(schurPoly.getArg(2)), 0);
+    EXPECT_EQ(std::real(schurPoly.getArg(0)), 0);
+    EXPECT_EQ(std::imag(schurPoly.getArg(0)), 0);
+    EXPECT_EQ(std::real(schurPoly.getArg(1)), 8);
+    EXPECT_EQ(std::imag(schurPoly.getArg(1)), 0);
+    EXPECT_EQ(std::real(schurPoly.getArg(2)), 16);
+    EXPECT_EQ(std::imag(schurPoly.getArg(2)), 0);
 }
